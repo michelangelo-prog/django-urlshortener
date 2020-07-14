9db94d69dda373b79780d0e2f333de9bb46c5a74
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-07-14 11:06:54 +0200

    initial commit

---
+++ urlshortener_app/config/settings/common.py
@@ -0,0 +1,94 @@
+import os
+from os.path import join
+from distutils.util import strtobool
+from configurations import Configuration
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Common(Configuration):
+
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+    DEBUG = strtobool(os.getenv("DJANGO_DEBUG", "no"))
+
+    ALLOWED_HOSTS = ["*"]
+
+    INSTALLED_APPS = (
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+    )
+
+    # https://docs.djangoproject.com/en/2.0/topics/http/middleware/
+    MIDDLEWARE = (
+        "django.middleware.security.SecurityMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+        "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    )
+
+    ROOT_URLCONF = "config.urls"
+
+    STATICFILES_DIRS = []
+
+    TEMPLATES = [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": STATICFILES_DIRS,
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.template.context_processors.debug",
+                    "django.template.context_processors.request",
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ],
+            },
+        },
+    ]
+
+    WSGI_APPLICATION = "config.wsgi.application"
+
+    # Postgres
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB", "urlshortener"),
+            "USER": os.getenv("POSTGRES_USER", "postgres"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres_password"),
+            "HOST": os.getenv("POSTGRES_HOST", "postgres"),
+            "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        }
+    }
+
+    # Password Validation
+    # https://docs.djangoproject.com/en/2.0/topics/auth/passwords/#module-django.contrib.auth.password_validation
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        },
+        {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+        {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+        {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    ]
+
+    LANGUAGE_CODE = "en-us"
+
+    TIME_ZONE = "UTC"
+
+    USE_I18N = False
+    USE_L10N = True
+    USE_TZ = True
+
+    STATIC_URL = "/static/"
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/2.0/howto/static-files/
+    STATIC_ROOT = os.path.normpath(join(os.path.dirname(BASE_DIR), "static"))
